90531345517957447b777b22b7e0c7473908a864
Elmeri Laitinen <elmeri.laitinen@cs.helsinki.fi>
2013-01-15 17:52:40 +0200

    projektikansio ja aiheen kuvaus lisatty

---
+++ Gladiaattoripeli/src/gladiaattoripeli/Gladiaattoripeli.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package gladiaattoripeli;
+
+/**
+ *
+ * @author Emleri
+ */
+public class Gladiaattoripeli {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
